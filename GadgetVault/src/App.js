d3ae96bc5ea3e4bcc5ea34cfb1dfaa6d41dd0eed
Anji515 <anji.gd17@gmail.com>
2023-03-29 09:58:40 +0530

    Store Added

--- GadgetVault/src/App.js
+++ GadgetVault/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>Welcome to GadgetVault</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
