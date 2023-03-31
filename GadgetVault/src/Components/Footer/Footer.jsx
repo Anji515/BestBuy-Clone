ae9173b682d6bb0aaa364b73c92ad50d4e228d2a
harshal3007 <harshalbhawsar9@gmail.com>
2023-03-31 19:02:07 +0530

    day4

--- GadgetVault/src/Components/Footer/Footer.jsx
+++ GadgetVault/src/Components/Footer/Footer.jsx
@@ -1,119 +1,168 @@
-import "./Footer.css"
-export const Footer = () =>{
+import {
+    Box,
+    Text,
+    Flex,
+    Input,
+    Button,
+    Heading,
+    Center,
+    UnorderedList,
+    Grid,
+    Avatar,
+  } from "@chakra-ui/react";
+  import styled from "styled-components";
+  
+  export function Footer() {
     return (
-        <>
-        <div className="footer">
-        <div className="sec_1" style={{display:"grid", gridTemplateColumns:"repeat(4,1fr)", justifyContent:"space-between"}}>
-            <div>
-                <div>
-                    <i class="fa-regular fa-circle-question font-60"></i>
-                </div>
-                <a href="/">Visit Our Support Center</a>
-            </div>
-            <div>
-                <div>
-                    <i class="fa-solid fa-dolly font-60"></i>
-                </div>
-                <a href="/">Check your Order Status</a>
-            </div>
-            <div>
-                <div>
-                    <i class="fa-solid fa-truck-fast font-60"></i>
-                </div>
-                <a href="/">Shipping, Delivery & Store Pickup</a>
-            </div>
-            <div>
-                <div>
-                    <i class="fa-solid fa-right-left font-60"></i>
-                </div>
-                <a href="/">Returns & Exchanges</a>
-            </div>
-        </div>
-        
-        <div className="sec_2" style={{display:"grid", gridTemplateColumns:"repeat(2,1fr)", width:"100%",justifyContent:"space-between"}}>
-            <div id="sec_2-1" style={{display:"grid", gridTemplateColumns:"repeat(3,1fr)",width:"100%"}}>
-                <div>
-                    <h3>Order & Purchases</h3>
-                    <p>Check Order Status</p>
-                    <p>Shipping, Delivery & Pickup</p>
-                    <p>Returns & Exchanges</p>
-                </div>
-                <div>
-                    <h3>Support & Services</h3>
-                    <p>Visit our Support Center</p>
-                    <p>Shop with an Expert</p>
-                    <p>Schedule a Service</p>
-                </div>
-                <div>
-                    <h3>Partnerships</h3>
-                    <p>Best Buy Totaltech</p>
-                    <p>My Best Buy</p>
-                    <p>View Points & Certificates</p>
-                    <p>Member Offers</p>
-                </div>
-                <div>
-                    <h3>Payment Options</h3>
-                    <p>Affiliate Program</p>
-                    <p>Advertise with Us</p>
-                    <p>Developers</p>
-                </div>
-                <div>
-                    <h3>Rewards & Membership</h3>
-                    <p>My Best Buy® Credit Card</p>
-                    <p>Pay Your Bill at Citibank</p>
-                    <p>Lease to Own</p>
-                </div>
-                <div>
-                    <h3>About Best Buy</h3>
-                    <p> Corporate Information</p>
-                    <p>Careers</p>
-                    <p>Corporate Responsibility & Sustainability</p>
-                    <p>Discover & Learn</p>
-                </div>
-            </div>
-            <div id="footer_form">
-                <p>Sign in or Create Account</p>
-                <div className="hl"></div>
-                <h3>Get the latest deals and more.</h3>
-                <input type="text" placeholder="Enter email Address"/>
-                <input type="submit" value="Sign Up"/>
-                <div className="hl"></div>
-                <div>
-                    <i class="fa-solid fa-mobile-button"></i>
-                    <span>Best Buy App </span>
-                </div>
-                <div className="hl"></div>
-                <div>
-                    <i class="fa-brands fa-facebook"></i>
-                    <i class="fa-brands fa-twitter"></i>
-                    <i class="fa-brands fa-instagram"></i>
-                    <i class="fa-brands fa-pinterest"></i>
-                    <i class="fa-brands fa-youtube"></i>
-                </div>
-                <p>Forums, blogs & more</p>
-            </div>
-        </div>
-        <div style={{marginLeft:"30px"}}>
-            <p>How was your experience?<span>Give feedback about our website</span></p>
-        </div>
-        <div className="hl"></div>
-        <div id="sec_3">
-            <div>
-                <p>Accessibility</p>
-                <p>Terms & Conditions</p>
-                <p>Privacy</p>
-                <p>Interest-Based Ads</p>
-                <p>California Privacy Rights</p>
-                <p>Do Not Sell My Personal Information</p>
-                <p>California Supply Chain Transparency Act</p>
-            </div>
-            <div>
-                In-store pricing may vary. Prices and offers are subject to change. © 2022 Best Buy. All rights
-                reserved. BEST BUY, the BEST BUY logo, the tag design, and MY BEST BUY are trademarks of Best Buy and
-                its affiliated companies.
-            </div>
-        </div>
-    </div>
-        </>
-    )
-}
+      <DIV mt={20}>
+        <Center>
+          <Flex
+            w={"100%"}
+            bg={"blackAlpha.100"}
+            padding={10}
+            margin={"auto"}
+            gap={20}
+            textAlign={["center"]}
+            direction={["column", "column", "row", "row"]}
+          >
+            <Box>
+              <Text>GET EMAIL OFFERS & THE LATEST NEWS FROM GadgetVault </Text>
+              <br />
+              <Text
+                textAlign={"left"}
+                whiteSpace={"nowrap"}
+                fontWeight={600}
+                fontSize={"lg"}
+              >
+                Enter Email
+              </Text>
+              <Input borderRadius={0} mb="5" border={"1px"} type="text" />
+  
+              <Button
+                bg={"blackAlpha.800"}
+                _hover={{ color: "black", bg: "blackAlpha.100" }}
+                color="white"
+              >
+                SUBMIT
+              </Button>
+              <br />
+              <br />
+              <Heading textAlign={"left"} mb="2" fontSize="md">
+                Get Connected
+              </Heading>
+              <Flex gap={5}>
+                <Avatar
+                  _hover={{ cursor: "pointer", boxShadow: "base" }}
+                  size={"sm"}
+                  src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png"
+                />
+                <Avatar
+                  size={"sm"}
+                  _hover={{ cursor: "pointer", boxShadow: "base" }}
+                  src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png"
+                />
+                <Avatar
+                  size={"sm"}
+                  _hover={{ cursor: "pointer", boxShadow: "base" }}
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYIKllja9ypDAIYMRNv6twRAvvNNJXIjInI2owtCQ&s"
+                />
+                <Avatar
+                  size={"sm"}
+                  _hover={{ cursor: "pointer", boxShadow: "base" }}
+                  src="https://w0.peakpx.com/wallpaper/813/107/HD-wallpaper-tiktok-logo.jpg"
+                />
+                <Avatar
+                  size={"sm"}
+                  _hover={{ cursor: "pointer", boxShadow: "base" }}
+                  src="https://www.freepnglogos.com/uploads/youtube-logo-hd-8.png"
+                />
+                <Avatar
+                  size={"sm"}
+                  _hover={{ cursor: "pointer", boxShadow: "base" }}
+                  src="https://pngimg.com/d/pinterest_PNG66.png"
+                />
+              </Flex>
+            </Box>
+            <Box>
+              <Grid
+                gap={10}
+                textAlign={["center"]}
+                templateColumns={{
+                  sm: "repeat(1,1fr)",
+                  md: "repeat(2,1fr)",
+                  lg: "repeat(4,1fr)",
+                }}
+              >
+                <UnorderedList className="list-items" textAlign={"left"}>
+                  <Text mb={2} fontSize={"lg"} fontWeight={600}>
+                    CUSTOMER CARE
+                  </Text>
+                  <Text>Help & FAQs</Text>
+                  <Text>Shipping</Text>
+                  <Text>Returns & Exchanges</Text>
+                  <Text>Order Tracking</Text>
+                  <Text>Corporate Sales & Gifts</Text>
+                  <Text>Contact Us</Text>
+                </UnorderedList>
+                <UnorderedList className="list-items" textAlign={"left"}>
+                  <Text mb={2} fontSize={"lg"} fontWeight={600}>
+                    MY ACCOUNT
+                  </Text>
+                  <Text>Sign In or Sign Up</Text>
+                  <Text>Order Tracking</Text>
+                  <Text>My Auto Refresh</Text>
+                  <Text>My Love-it List</Text>
+                </UnorderedList>
+                <UnorderedList className="list-items" textAlign={"left"}>
+                  <Text mb={2} fontSize={"lg"} fontWeight={600}>
+                    DISCOVER
+                  </Text>
+                  <Text>About Us</Text>
+                  <Text>Careers</Text>
+                  <Text>Gift Cards</Text>
+                  <Text>Shop by Fragrance</Text>
+                  <Text>Product Ingredients</Text>
+                  <Text>Get Inspired</Text>
+                </UnorderedList>
+                <UnorderedList className="list-items" textAlign={"left"}>
+                  <Text mb={2} fontSize={"lg"} fontWeight={600}>
+                    FIND US
+                  </Text>
+                  <Text>Store Locator</Text>
+                  <Text>Global Locations</Text>
+                  <Text>Returns & Exchanges</Text>
+                  <Text>Order Tracking</Text>
+                  <Text>Corporate Sales & Gifts</Text>
+                  <Text>Contact Us</Text>
+                </UnorderedList>
+              </Grid>
+            </Box>
+          </Flex>
+        </Center>
+        <Box p={10} bg={"blackAlpha.200"}>
+          <Text>
+            Terms of use | Privacy Policy | Security Bug Report | States Privacy
+            Rights Notice (Certain States) | Do Not Sell or Share My Personal
+            Information (Certain States) | Transparency in Supply Chains |
+            Targeted Ad Preferences (Certain States) | Limit the Use of My
+            Sensitive Personal Information (California)
+          </Text>
+          <br />
+          <br />
+          <br />
+          <Text>© 2023 GadgetVault, Inc. All Rights Reserved.</Text>
+        </Box>
+      </DIV>
+    );
+  }
+  
+  let DIV = styled.div`
+    .list-items > p {
+      padding: 10px;
+      cursor: pointer;
+    }
+    .list-items > p:hover {
+      text-decoration: underline;
+    }
+  `;
+  
